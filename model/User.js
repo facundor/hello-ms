908875478917bb2e658b27264aec23ec4b278ace
Facundo Rodriguez <facundo.rodriguez@fluxit.com.ar>
2020-04-06 09:12:47 -0300

    agrego persistencia al usuario

--- model/User.js
+++ model/User.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-  id: {
-    type: Number,
-    required: true
-  },
   name: {
     type:String,
     required: true
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../model/User')
-const mongoose = require('mongoose');
+const User = require("../model/User");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
-  let user = { id: 304, name: "Charles", surname: "Darwin"};
-  res.status(200).end(JSON.stringify(user));
+router.get("/", function (req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  let user = { name: "Charles", surname: "Darwin" };
+  res.status(200).json(user);
 });
 
 /* POST user. */
-router.post('/', function(req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
-  let user = req.body
-  mongoose.connection.
-  res.status(200).end(JSON.stringify(user));
+router.post("/", function (req, res, next) {
+  const user = User({
+    name: req.body.name,
+    surname: req.body.surname,
+  });
+
+  user
+    .save()
+    .then((data) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.log("error: " + err);
+      res.json({ message: err });
+    });
 });
 
 module.exports = router;
